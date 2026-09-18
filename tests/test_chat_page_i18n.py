4557496aa600490fb494d03af7d860e41033d480
Dovlet Babajanov <dowletbabajanov165@gmail.com>
2026-09-18 21:17:39 +0200

    fix: complete German chat localization

--- tests/test_chat_page_i18n.py
+++ tests/test_chat_page_i18n.py
@@ -1,4 +1,5 @@
 import app
+from backend.i18n import UI_TRANSLATIONS
 from backend.models import User
 from io import BytesIO
 from werkzeug.datastructures import FileStorage
@@ -330,3 +331,28 @@ def test_upload_validator_enforces_per_category_size_limits(monkeypatch):
 
     assert app.allowed_mime_type(allowed_image) is True
     assert app.allowed_mime_type(oversized_image) is False
+def test_reviewed_german_chat_has_no_english_fallbacks():
+    chat_keys = {
+        "you", "call_ended", "original_message", "unblock", "unrestrict",
+        "write_message", "unavailable", "open_chat", "messages_intro",
+        "active_dialogs", "new_conversation", "no_active_dialogs",
+        "messages_unavailable", "messages_restricted_intro", "restricted_user",
+        "restricted_user_notice", "typing_message", "media_file", "pinned",
+        "unpin", "voice_message", "video_call", "audio_call", "call_missed",
+        "call_declined", "call_accepted", "call", "forwarded_message", "edited",
+        "chat_search", "ai_message_translation_notice", "incoming_call",
+        "user_is_calling", "accept", "decline", "search_messages", "previous",
+        "next", "close", "enter_search_text", "reply_to_message",
+        "editing_message", "voice_recording", "cancel", "send", "file",
+        "write_message_placeholder", "chat_hint", "back_to_dashboard",
+        "delete_for_me", "delete_for_everyone", "reply", "edit", "forward",
+        "copy", "translate", "pin", "info", "sent_at", "incoming_video_call",
+        "incoming_audio_call", "user", "is_calling_you", "search_no_results",
+        "search_found", "search_current", "voice_not_supported",
+        "microphone_error", "voice_sending", "original", "call_quality_offline",
+        "push_open_call", "browser_session",
+    }
+    assert {
+        key for key in chat_keys
+        if UI_TRANSLATIONS["de"][key] == UI_TRANSLATIONS["en"][key]
+    } == set()
